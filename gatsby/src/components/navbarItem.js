3cc4fd710691941cd62c18ed146dce33ffe610d1
jmolivas <jesus.olivas@gmail.com>
2019-05-21 01:35:01 -0700

    feat: add navbar component

--- gatsby/src/components/navbarItem.js
+++ gatsby/src/components/navbarItem.js
@@ -4,17 +4,19 @@ import { Link } from "gatsby"
 const NavbarItem = ({ item }) => {
   let children = ""
   if (item.items) {
-    children = item.items.map(item => {
-      return (
-        <li key={`${item.id}-li-child`} id={`${item.id}-li-child`}>
-          <ul id="manual-guide-toc" className="manual-guide-toc child">
-            <li key={`${item.id}-li-key`} id={`${item.id}-li`}>
-              <Link to={item.link}>{item.title}</Link>
-            </li>
-          </ul>
-        </li>
-      )
-    })
+    children = (
+      <li key={`${item.id}-li-child`} id={`${item.id}-li-child`}>
+        <ul id="manual-guide-toc-child" className="manual-guide-toc child">
+          {item.items.map(item => {
+            return (
+              <li key={`${item.id}-li-key`} id={`${item.id}-li`}>
+                <Link to={item.link}>{item.title}</Link>
+              </li>
+            )
+          })}
+        </ul>
+      </li>
+    )
   }
 
   return (
